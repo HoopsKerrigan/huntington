4cffe64045852c453666f70c9d0e6f96a1ac88cb
HoopsKerrigan <112337422+HoopsKerrigan@users.noreply.github.com>
2022-08-30 21:42:01 -0400

    Update login-page.js

--- login-page.js
+++ login-page.js
@@ -7,7 +7,7 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "user" && password === "web_dev") {
+    if (username === "HPD_HBurnha" && password === "1643246") {
         alert("You have successfully logged in.");
         location.reload();
     } else {
