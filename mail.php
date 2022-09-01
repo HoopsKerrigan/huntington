2591ae29f4801b70bb4e7f2a09b766010ffbb80d
HoopsKerrigan <112337422+HoopsKerrigan@users.noreply.github.com>
2022-09-01 01:43:17 -0400

    Update mail.php

--- mail.php
+++ mail.php
@@ -5,6 +5,6 @@ $formcontent="From: $name \n Message: $message";
 $recipient = "maccoykerrigan@gmail.com";
 $subject = "Newsletter Contact Form";
 $mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+mail('maccoykerrigan@gmail.com', $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!";
 ?>
