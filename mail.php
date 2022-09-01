e6fd3ab860aee49bfd8aaf7ca1135440ae5ac504
HoopsKerrigan <112337422+HoopsKerrigan@users.noreply.github.com>
2022-09-01 01:38:46 -0400

    Create mail.php

---
+++ mail.php
@@ -0,0 +1,10 @@
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "maccoykerrigan@gmail.com";
+$subject = "Newsletter Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
