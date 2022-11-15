0da99054663ed037aa5307456f2890a254061506
HoopsKerrigan <112337422+HoopsKerrigan@users.noreply.github.com>
2022-11-15 13:33:05 -0500

    Create police-login-page.js

---
+++ police-login-page.js
@@ -0,0 +1,18 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+
+    if (username === "95-006129") {
+        alert("You have successfully logged in.");
+	window.location.assign("https://cityofhuntingtonwv.com/HPDCase95006129.html");
+    } else if (username === "95-006145") {
+        alert("You have successfully logged in.");
+	window.location.replace("https://cityofhuntingtonwv.com/HPDCase95006145.html");
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
